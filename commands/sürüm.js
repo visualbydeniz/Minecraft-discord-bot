899ee8536109fd1bcf0ca05246cf3abad1344440
Deniz Ağlar <130601088+visualbydeniz@users.noreply.github.com>
2026-08-27 18:49:56 +0300

    Update sürüm.js

--- commands/sürüm.js
+++ commands/sürüm.js
@@ -7,8 +7,8 @@ module.exports = {
       .addField(`** **`, `**> 1.16 - 1.18**`) // // Versions that can connect to the server
       .addField(`>  **Recommended Version →  1.17.1 Optifine**`, `** **`) // // Recommended version to use
       .setColor('YELLOW')
-      .setThumbnail(`https://cdn.discordapp.com/attachments/1083829077774966787/1085197496617283604/blokolikkkk.png`) // // You can replace this image with your own branding
-      .setImage(`https://cdn.discordapp.com/attachments/860830300560293911/1085613102751567912/blokolik22.png`) // // Background image/banner (also replaceable)
+      .setThumbnail(``) // // You can replace this image with your own branding
+      .setImage(``) // // Background image/banner (also replaceable)
       .setTimestamp()
       .setFooter('PixelCraft Bot', client.user.avatarURL()) // // Changed from 'Blokolik Bot' to 'PixelCraft Bot'
     
